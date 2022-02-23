50d8af0f2874e6548f027e6b46018692d2ab46f3
juancode-espi <juan.pdr17@gmail.com>
2022-02-23 14:38:59 -0500

    API Access and cosume service back

--- src/main/java/edu/escuelaing/arep/HttpServer.java
+++ src/main/java/edu/escuelaing/arep/HttpServer.java
@@ -3,7 +3,6 @@ import java.net.*;
 import java.nio.charset.Charset;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,14 +10,26 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
-import java.nio.charset.Charset;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class HttpServer {
+    private static final String HTTP_MESSAGE = "HTTP/1.1 200 OK \r\n"
+            + "Content-Type: text/html" + "\r\n"
+            + "\r\n";
 
-    private static final String WHEATER_QUERY = "https://api.openweathermap.org/data/2.5/weather?q=country&appid=d1bcfbab47d918a819df1b59af4eee93";
+    private static final String CSS_MESSAGE = "HTTP/1.1 200 OK \r\n"
+            + "Content-Type: text/css" + "\r\n"
+            + "\r\n";
+    private static final String JS_MESSAGE = "HTTP/1.1 200 OK \r\n"
+            + "Content-Type: text/javascipt" + "\r\n"
+            + "\r\n";
+
+    private static final String HTTP_MESSAGE_NOT_FOUND = "HTTP/1.1 404 Not Found\n"
+            + "Content-Type: text/html\r\n"
+            + "\r\n";
+    private static final String WHEATER_QUERY = "https://api.openweathermap.org/data/2.5/weather?q=country&appid=1dc647740ce39a8ad83463a91a3450c8";
 
     private static final HttpServer _instance = new HttpServer();
 
@@ -29,7 +40,7 @@ public class HttpServer {
     private HttpServer(){
     }
 
-    public String start() throws IOException{
+    public String connect() throws IOException{
         ServerSocket serverSocket = null;
         String jsonString="VACIOS";
         try {
@@ -98,9 +109,101 @@ public class HttpServer {
 
         return outputLine;
     }
+    public String getResource( String uri, OutputStream outStream) throws URISyntaxException{
 
+        if(uri.contains("clima")){
+            //System.out.println(computeHTMLResponse(OutputStream out));
+            return computeHTMLResponse(outStream);
+        }else if(uri.contains("consulta")){
+            String country = uri.substring(uri.lastIndexOf("=") + 1);
+            System.out.println("country "+country);
+            try {
+                return computeJSONResponse(country);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }else if(uri.contains("css")){
+            return computeCSSResponse(outStream);
+        }else if(uri.contains("js")){
+            return computeJSResponse(outStream);
+        }
+        return null;
+    }
 
+    public static String computeJSONResponse(String country) throws IOException{
+        String jsonText = HTTP_MESSAGE.replaceFirst("text", "application").replaceFirst("html", "json"); int cp;
+        InputStream is = new URL(WHEATER_QUERY.replaceFirst("country", country)).openStream();
+        BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+        while ((cp = rd.read()) != -1) jsonText += (char) cp;
+        return jsonText;
+    }
 
+    public String computeHTMLResponse(OutputStream out){
+        String content = HTTP_MESSAGE;
+        File file = new File("src/main/resources/public/index.html");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line =  br.readLine()) != null) content += line + "\n";
+            br.close();
+            out.write(content.getBytes());
+        } catch (IOException e) {
+            System.err.format("FileNotFoundException %s%n", e);
+            default404HTMLResponse(out);
+        }
+        return content;
+    }
+    public String computeJSResponse(OutputStream out){
+        String content = JS_MESSAGE;
+        File file = new File("src/main/resources/public/index.html");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line =  br.readLine()) != null) content += line + "\n";
+            br.close();
+            out.write(content.getBytes());
+        } catch (IOException e) {
+            System.err.format("FileNotFoundException %s%n", e);
+            default404HTMLResponse(out);
+        }
+        return content;
+    }
+    public String computeCSSResponse(OutputStream out){
+        String content = CSS_MESSAGE;
+        File file = new File("src/main/resources/public/index.html");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while((line =  br.readLine()) != null) content += line + "\n";
+            br.close();
+            out.write(content.getBytes());
+        } catch (IOException e) {
+            System.err.format("FileNotFoundException %s%n", e);
+            default404HTMLResponse(out);
+        }
+        return content;
+    }
+    private void default404HTMLResponse(OutputStream out){
+        String outputline = HTTP_MESSAGE_NOT_FOUND;
+        outputline +=     "<!DOCTYPE html>"
+                + "<html>"
+                +       "<head>"
+                +           "<title>404 Not Found</title>\n"
+                +           "<meta charset=\"UTF-8\">"
+                +           "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.8\">"
+                +       "</head>"
+                +       "<body>"
+                +           "<h1> Error 404 </h1>"
+                +       "</body>"
+                + "</html>";
+        try {
+            out.write(outputline.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public String computeDefaultResponse(){
@@ -126,12 +229,29 @@ public class HttpServer {
         }
         return 35000; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
 
 
 
-
-
+    public static JSONObject readJsonFromUrl(String city) throws IOException, JSONException {
+        InputStream is = new URL(city).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
     public static void main(String[] args) throws IOException {
-        HttpServer.getInstance().start();
+        HttpServer.getInstance().connect();
     }
 }
